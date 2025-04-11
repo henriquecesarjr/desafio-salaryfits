e04e292f2115a4f84116c4ca2771df65f88d4aa0
Henrique Marques <132225341+henriquecesarjr@users.noreply.github.com>
2025-04-11 19:57:40 -0300

    feat: implement book update

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('books')
 export class BookController {
@@ -9,5 +10,10 @@ export class BookController {
   @Post()
   createBook(@Body() dto: CreateBookDto) {
     return this.bookService.createBook(dto);
+  }
+
+  @Patch(":id")
+  updateBook(@Param("id") id: string, @Body() dto: UpdateBookDto) {
+    return this.bookService.updateBook(id, dto)
   }
 }
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
@@ -20,5 +20,22 @@ export class BookService {
     return newBook;
   }
 
-  
+  async updateBook(id: string, dto: UpdateBookDto) {
+    const findBook = await this.prisma.book.findFirst({
+      where: { id },
+    })
+
+    if(!findBook) {
+      throw new HttpException("O livro não foi encontrado!", HttpStatus.NOT_FOUND);
+    }
+
+    const updatedBook = await this.prisma.book.update({
+      where: {
+        id: findBook.id,
+      },
+      data: dto,
+    })
+
+    return updatedBook;
+  }
 }
