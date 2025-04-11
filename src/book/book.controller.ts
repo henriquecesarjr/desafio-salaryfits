2705bd89d98c7c03d22e535816f3f5c938677d06
Henrique Marques <132225341+henriquecesarjr@users.noreply.github.com>
2025-04-11 19:43:43 -0300

    feat: implement book creation

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,5 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { BookService } from './book.service';
+import { CreateBookDto } from './dto/create-book.dto';
 
-@Controller('book')
-export class BookController {}
+@Controller('books')
+export class BookController {
+  constructor(private bookService: BookService) {}
+
+  @Post()
+  createBook(@Body() dto: CreateBookDto) {
+    return this.bookService.createBook(dto);
+  }
+}
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+  constructor(private prisma: PrismaService) {}
+
+  async createBook(dto: CreateBookDto) {
+    const newBook = await this.prisma.book.create({
+      data: {
+        title: dto.title,
+        author: dto.author,
+        description: dto.description,
+        year: dto.year
+      }
+    })
+
+    return newBook;
+  }
+
+  
+}
