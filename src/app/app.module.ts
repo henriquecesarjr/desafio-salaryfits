c5bd782b368b76b40dbb39c69f487dbfc96196f2
Henrique Marques <132225341+henriquecesarjr@users.noreply.github.com>
2025-04-13 21:07:17 -0300

    refactor: remove unused files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from 'src/auth/auth.module';
 import { BookModule } from 'src/book/book.module';
-import { AuthAdminGuard } from 'src/common/guards/admin.guard';
-import { LoggerMiddleware } from 'src/common/middlewares/logger.middlware';
 import { ReviewModule } from 'src/review/review.module';
 import { UsersModule } from 'src/users/users.module';
 
@@ -19,12 +17,4 @@ import { UsersModule } from 'src/users/users.module';
   controllers: [],
   providers: [],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-      consumer.apply(LoggerMiddleware)
-      .forRoutes({
-        path: "*",
-        method: RequestMethod.ALL
-      })
-  }
-}
+export class AppModule {}
--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -2,22 +2,12 @@ import { Module } from '@nestjs/common';
 import { BookController } from './book.controller';
 import { BookService } from './book.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { ApiExceptionFilter } from 'src/common/filters/exception-filter';
-import { APP_FILTER } from '@nestjs/core';
 
 @Module({
   imports: [PrismaModule],
   controllers: [BookController],
   providers: [
-    BookService,
-    {
-      provide: APP_FILTER,
-      useClass: ApiExceptionFilter
-    },
-    {
-      provide: "KEY_TOKEN",
-      useValue: "TOKEN_123456789"
-    }
+    BookService
   ]
 })
 export class BookModule {}
